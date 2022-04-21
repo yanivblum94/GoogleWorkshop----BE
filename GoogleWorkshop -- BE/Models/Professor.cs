3637b83729a5f2df9e619f974c53f58d8f433e7d
Yaniv Blum <jungob94@gmail.com>
2022-04-21 17:13:44 +0300

    Added new fields for review and prof and updated add review method

--- GoogleWorkshop -- BE/Models/Professor.cs
+++ GoogleWorkshop -- BE/Models/Professor.cs
@@ -13,22 +13,34 @@ namespace GoogleWorkshop____BE.Models
         public string Faculty { get; set; }
         public double TotalRating { get; set; }
         public double DiffRating { get; set; }
+        public double TreatRating { get; set; }
+        public double MaterialsUpdateOdds { get; set; }
+        public double RecordsUpdateOdds { get; set; }
+        public double TakeAgainOdds { get; set; }
         public string EmailAddr { get; set; }
         public string WebsiteAddr { get; set; }
         public string LinkedinProfile { get; set; }
         public string TwitterProfile { get; set; }
+        public List<string> Courses { get; set; }
         public List<Review> Reviews { get; set; }
 
         public Professor() { }
 
-        public Professor(string name, string faculty, double totalRating, double diffRating, string emailAddr, string websiteAddr)
+        public Professor(string name, string faculty, double totalRating, double diffRating, double treatRating, double materialsUpdateOdds, double recordsUpdateOdds, 
+            double takeAgainOdds, string emailAddr, string websiteAddr, string linkedinProfile, string twitterProfile)
         {
             Name = name;
             Faculty = faculty;
             TotalRating = totalRating;
             DiffRating = diffRating;
+            TreatRating = treatRating;
+            MaterialsUpdateOdds = materialsUpdateOdds;
+            RecordsUpdateOdds = recordsUpdateOdds;
+            TakeAgainOdds = takeAgainOdds;
             EmailAddr = emailAddr;
             WebsiteAddr = websiteAddr;
+            LinkedinProfile = linkedinProfile;
+            TwitterProfile = twitterProfile;
         }
 
         public override bool Equals(object obj)
@@ -38,10 +50,14 @@ namespace GoogleWorkshop____BE.Models
                    string.Equals(Faculty, professor.Faculty) &&
                    TotalRating == professor.TotalRating &&
                    DiffRating == professor.DiffRating &&
+                   MaterialsUpdateOdds == professor.MaterialsUpdateOdds &&
+                   RecordsUpdateOdds == professor.RecordsUpdateOdds &&
+                   TakeAgainOdds == professor.TakeAgainOdds &&
                    string.Equals(EmailAddr, professor.EmailAddr) &&
                    string.Equals(WebsiteAddr, professor.WebsiteAddr) &&
                    string.Equals(TwitterProfile, professor.TwitterProfile) &&
                    string.Equals(LinkedinProfile, professor.LinkedinProfile) &&
+                   Enumerable.SequenceEqual(Courses.OrderBy(e => e), professor.Courses.OrderBy(e => e)) &&
                    Enumerable.SequenceEqual(Reviews.OrderBy(e=>e), professor.Reviews.OrderBy(e => e));
         }
 
@@ -49,22 +65,37 @@ namespace GoogleWorkshop____BE.Models
         {
             if (this.Reviews == null)
                 this.Reviews = new List<Review>();
+            if (this.Courses == null)
+                this.Courses = new List<string>();
             this.Reviews.Add(rev);
+            if (!this.Courses.Contains(rev.Course))
+                this.Courses.Add(rev.Course);
             CalcRating();
         }
 
 
         public void CalcRating()
         {
-            int sum1 = 0, sum2=0;
+            int ratingSum = 0, diffSum = 0, treatSum = 0, materialsTrues = 0, recordsTrues = 0, takeAgainnTrues = 0;
             foreach (Review rev in this.Reviews)
             {
-                sum1 += rev.TotalRating;
-                sum2 += rev.DiffRating;
+                ratingSum += rev.TotalRating;
+                diffSum += rev.DiffRating;
+                treatSum += rev.TreatRating;
+                if (rev.MaterialsUpdate)
+                    materialsTrues++;
+                if (rev.RecordsUpdate)
+                    recordsTrues++;
+                if (rev.TakeAgain)
+                    takeAgainnTrues++;
             }
-
-            this.TotalRating = (double)(sum1 / this.Reviews.Count);
-            this.DiffRating = (double)(sum2 / this.Reviews.Count);
+            var count = (double)this.Reviews.Count;
+            this.TotalRating = (double)(ratingSum / count);
+            this.DiffRating = (double)(diffSum / count);
+            this.TreatRating = (double)(treatSum / count);
+            this.MaterialsUpdateOdds = (double)(materialsTrues / count);
+            this.RecordsUpdateOdds = (double)(recordsTrues / count);
+            this.TakeAgainOdds = (double)(takeAgainnTrues / count);
         }
     }
 }
--- GoogleWorkshop -- BE/Models/Review.cs
+++ GoogleWorkshop -- BE/Models/Review.cs
@@ -12,15 +12,23 @@ namespace GoogleWorkshop____BE.Models
         public ObjectId ID { get; set; }
         public int TotalRating { get; set; }
         public int DiffRating { get; set; }
+        public int TreatRating { get; set; }
+        public bool MaterialsUpdate { get; set; }
+        public bool RecordsUpdate { get; set; }
+        public bool TakeAgain { get; set; }
         public string Comment { get; set; }
         public string Course { get; set; }
 
         public Review() { }
 
-        public Review(int totalRating, int diffRating, string comment, string course)
+        public Review(int totalRating, int diffRating, int treatRating, bool materialsUpdate, bool recordsUpdate, bool takeAgain, string comment, string course)
         {
             TotalRating = totalRating;
             DiffRating = diffRating;
+            TreatRating = treatRating;
+            MaterialsUpdate = materialsUpdate;
+            RecordsUpdate = recordsUpdate;
+            TakeAgain = takeAgain;
             Comment = comment;
             Course = course;
         }
@@ -30,6 +38,10 @@ namespace GoogleWorkshop____BE.Models
             return obj is Review review &&
                    TotalRating == review.TotalRating &&
                    DiffRating == review.DiffRating &&
+                   TreatRating == review.TreatRating &&
+                   MaterialsUpdate == review.MaterialsUpdate &&
+                   RecordsUpdate == review.RecordsUpdate &&
+                   TakeAgain == review.TakeAgain &&
                    string.Equals(Comment, review.Comment) &&
                    string.Equals(Course, review.Course);
         }
