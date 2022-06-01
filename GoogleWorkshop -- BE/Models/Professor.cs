0ab9b741eebf951d152f56e3c8cf41826dea83ba
Yaniv Blum <jungob94@gmail.com>
2022-06-01 09:25:54 +0300

    changes str compare

--- GoogleWorkshop -- BE/Models/Professor.cs
+++ GoogleWorkshop -- BE/Models/Professor.cs
@@ -120,7 +120,7 @@ namespace GoogleWorkshop____BE.Models
             {
                 for (int i = 0; i < this.Courses.Count; i++)
                 {
-                    if (this.Courses[i] == rev.Course)
+                    if (string.Equals(this.Courses[i], rev.Course))
                     {
                         courseReviewCount[i]++;
                         courseDiffSum[i] += rev.DiffRating;
